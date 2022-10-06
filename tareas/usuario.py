de68e25a5c62aeed981b42f6bc67334a461e475f
Josefa Grandon <jovagran@gmail.com>
2022-10-06 08:24:32 -0300

    avance

--- tareas/usuario.py
+++ tareas/usuario.py
@@ -30,13 +30,31 @@ mayna = Usuario("mayna", "mayna@gmail.com")
 
 #Haz que el primer usuario haga 3 depósitos y 1 giro, y luego muestra sus balances
 jazz.hacer_deposito(400)
-jazz.hacer_retiro(100)
+jazz.hacer_deposito(300)
+jazz.hacer_deposito(200)
+jazz.hacer_retiro(500)
 jazz.mostrar_balance_usuario()
 
+#Haz que el segundo usuario haga 2 depósitos y 2 giros, y luego muestra sus balances
 marcelo.hacer_deposito(1000)
+marcelo.hacer_deposito(1000)
+marcelo.hacer_retiro(400)
+marcelo.hacer_retiro(400)
 marcelo.mostrar_balance_usuario()
 
-marcelo.transfer_dinero(jazz,800)
 
+
+# Haz que el tercer usuario haga 1 depósito y 3 giros, y luego muestra sus balances
+mayna.hacer_deposito(2000)
+mayna.hacer_retiro(200)
+mayna.hacer_retiro(300)
+mayna.hacer_retiro(300)
+mayna.mostrar_balance_usuario()
+
+
+# BONUS: Agrega un método transferir_dinero; haz que el primer usuario transfiera dinero al tercer usuario y luego imprime los balances de ambos usuarios
+
+
+mayna.transfer_dinero(jazz,800)
 jazz.mostrar_balance_usuario()
 marcelo.mostrar_balance_usuario()
